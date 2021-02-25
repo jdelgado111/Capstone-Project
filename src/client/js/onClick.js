4d3fe6b72111e4e378bb4e9c8365dadb4b081824
jdelgado111 <jdm41091@gmail.com>
2021-02-25 14:36:35 -0800

    Geonames API working with user input

--- src/client/js/onClick.js
+++ src/client/js/onClick.js
@@ -1,13 +1,15 @@
-function handleClick(event) {
-    console.log("Calling fetch");
-    fetch("http://api.geonames.org/searchJSON?q=london&maxRows=1&username=jdelgado")
-        .then(res => res.json())
-        .then(json => {
-            //console.log("Calling updateUI");
-            //updateUI(json);
-            console.log(json);
-            console.log(json.geonames[0].countryName);
-        });
+async function handleClick(event) {
+    const city = document.getElementById("city").value;
+
+    console.log("Calling Geonames API");
+    const json = await fetch("http://api.geonames.org/searchJSON?q="+city+"&maxRows=1&username=jdelgado")
+    .then(res => res.json());
+
+    console.log(json);
+    console.log(json.geonames[0].countryName);
+
+    //console.log("Calling Weatherbit API");
+
 }
 
 export { handleClick }
