0c5eb31784f259001b711d52a6db2474e305f07f
jdelgado111 <jdm41091@gmail.com>
2021-02-25 15:48:31 -0800

    Minor changes

--- src/client/js/onClick.js
+++ src/client/js/onClick.js
@@ -1,12 +1,19 @@
 async function handleClick(event) {
     const city = document.getElementById("city").value;
 
-    console.log("Calling Geonames API");
-    const json = await fetch("http://api.geonames.org/searchJSON?q="+city+"&maxRows=1&username=jdelgado")
-    .then(res => res.json());
+    const geoUrl = "http://api.geonames.org/searchJSON?q="+city+"&maxRows=1&username=";
+    const geoKey = "jdelgado";
 
+    const weatherUrl = "";
+    const weatherKey = "394d151bb3f448218aab367dcc64e3b3";
+
+    console.log("Calling Geonames API");
+
+    const json = await fetch(geoUrl+geoKey)
+    .then(res => res.json());
     console.log(json);
     console.log(json.geonames[0].countryName);
+
 
     //console.log("Calling Weatherbit API");
 
