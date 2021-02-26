d381d956b4b573c7d9dd71dd7ba07826f9fd6b8c
jdelgado111 <jdm41091@gmail.com>
2021-02-25 16:19:07 -0800

    Weatherbit API is working

--- src/client/js/onClick.js
+++ src/client/js/onClick.js
@@ -4,19 +4,41 @@ async function handleClick(event) {
     const geoUrl = "http://api.geonames.org/searchJSON?q="+city+"&maxRows=1&username=";
     const geoKey = "jdelgado";
 
-    const weatherUrl = "";
-    const weatherKey = "394d151bb3f448218aab367dcc64e3b3";
-
     console.log("Calling Geonames API");
 
-    const json = await fetch(geoUrl+geoKey)
+    const geoJson = await fetch(geoUrl+geoKey)
     .then(res => res.json());
-    console.log(json);
-    console.log(json.geonames[0].countryName);
+    console.log(geoJson);
 
+    const geoData = geoJson.geonames[0];
+    console.log(geoData.countryName);
 
-    //console.log("Calling Weatherbit API");
+    const lat = geoData.lat;
+    const lng = geoData.lng;
 
+    //TODO: check how near or far date is from today: <=7 days is near, >7 days is far
+
+    const weatherUrlNear = "http://api.weatherbit.io/v2.0/current?&lat="+lat+"&lon="+lng+"&key=";
+    const weatherUrlFar = "https://api.weatherbit.io/v2.0/forecast/daily?&lat="+lat+"&lon="+lng+"&key=";
+    const weatherKey = "394d151bb3f448218aab367dcc64e3b3";
+    
+    console.log("Calling Weatherbit API");
+
+    const weatherJson1 = await fetch(weatherUrlNear+weatherKey)
+    .then(res => res.json());
+    console.log(weatherJson1);
+
+    const weatherData1 = weatherJson1.data[0];
+    console.log(weatherData1.temp);
+
+    const weatherJson2 = await fetch(weatherUrlFar+weatherKey)
+    .then(res => res.json());
+    console.log(weatherJson2);
+
+    const weatherData2 = weatherJson2.data[0];
+    console.log(weatherData2.temp);
+
+    //console.log("Calling Pixabay API");
 }
 
 export { handleClick }
