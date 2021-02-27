2f90cf330a1d6f6463441cf724a1de816d9d987e
jdelgado111 <jdm41091@gmail.com>
2021-02-26 22:32:06 -0800

    Added service workers

--- src/client/index.js
+++ src/client/index.js
@@ -7,5 +7,22 @@ import "./styles/style.scss";
 //event listener
 document.getElementById("btn").addEventListener("click", handleClick);
 
+//service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+ 
+      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+ 
+        console.log('SW registered: ', registration);
+ 
+      }).catch(registrationError => {
+ 
+        console.log('SW registration failed: ', registrationError);
+ 
+      });
+ 
+    });
+  }
+
 //export functions
 export { handleClick, updateUI }
